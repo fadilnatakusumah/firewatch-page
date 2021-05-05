995047812711d6c1ca0471c03d99a24ce56e3819
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-05-05 16:03:11 +0700

    update to simple paralax

--- js/main.js
+++ js/main.js
@@ -1,62 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const controller = new ScrollMagic.Controller();
-  const timeline = new TimelineMax();
+const image1 = document.getElementById("parallax-1")
+const image2 = document.getElementById("parallax-2")
+const image3 = document.getElementById("parallax-3")
+const image4 = document.getElementById("parallax-4")
+const image5 = document.getElementById("parallax-5")
+const image6 = document.getElementById("parallax-6")
+const image7 = document.getElementById("parallax-7")
+const image8 = document.getElementById("parallax-8")
+const image9 = document.getElementById("parallax-9")
 
-  timeline
-    .to('#parallax-1, #parallax-helper', 6, {
-      y: -750,
-    })
-    .to('#parallax-2', 6, {
-      y: -600,
-    }, '-=6')
-    .to('#parallax-3', 6, {
-      y: -500,
-    }, '-=6')
-    .to('#parallax-4', 6, {
-      y: -400,
-    }, '-=6')
-    .to('#parallax-5', 6, {
-      y: -300,
-    }, '-=6')
-    .to('#parallax-6', 6, {
-      y: -200,
-    }, '-=6')
-    .to('#parallax-7', 6, {
-      y: -100,
-    }, '-=6')
-    .to('#parallax-8', 6, {
-      y: -50,
-    }, '-=6')
-    .to('#parallax-9', 6, {
-      y: -25,
-    }, '-=6')
-    .to('.content', 6, {
-      top: '0%',
-    }, '-=6')
-    .to('header', 6, {
-      y: -100,
-      opacity: .05
-    }, '-=6')
-  // .to('#fifth', 6, {
-  //   y: -500,
-  // }, '-=6')
-  // .to('#fourth', 6, {
-  //   y: -400,
-  // }, '-=6')
-  // .to('#third', 6, {
-  //   y: -300,
-  // }, '-=6')
-  // .to('#second', 6, {
-  //   y: -200,
-  // }, '-=6')
-  // .to('#first', 6, {})
+const onScrolling = ()=> {
+  const offset =window.pageYOffset;
+  image2.style.transform = `translateY(${(offset * .1)}px)`;
+  image3.style.transform = `translateY(${(offset * .2)}px)`;
+  image4.style.transform = `translateY(${(offset * .3)}px)`;
+  image5.style.transform = `translateY(${(offset * .4)}px)`;
+  image6.style.transform = `translateY(${(offset * .5)}px)`;
+  image7.style.transform = `translateY(${(offset * .6)}px)`;
+  image8.style.transform = `translateY(${(offset * .7)}px)`;
+  image9.style.transform = `translateY(${(offset * .8)}px)`;
+}
 
-  new ScrollMagic.Scene({
-    trigger: 'section',
-    duration: '200%',
-    triggerHook: 0,
-  })
-    .setTween(timeline)
-    .setPin('section')
-    .addTo(controller);
-})
+document.addEventListener("scroll", onScrolling)
