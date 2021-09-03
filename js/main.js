0e6fa0f2da3edb184886ea718622adbcdc6feabf
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-09-03 23:06:41 +0700

    add mobile bg, and clean up style

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-const image1 = document.getElementById("parallax-1")
-const image2 = document.getElementById("parallax-2")
-const image3 = document.getElementById("parallax-3")
-const image4 = document.getElementById("parallax-4")
-const image5 = document.getElementById("parallax-5")
-const image6 = document.getElementById("parallax-6")
-const image7 = document.getElementById("parallax-7")
-const image8 = document.getElementById("parallax-8")
-const image9 = document.getElementById("parallax-9")
+const image1 = document.getElementById("parallax-1");
+const image2 = document.getElementById("parallax-2");
+const image3 = document.getElementById("parallax-3");
+const image4 = document.getElementById("parallax-4");
+const image5 = document.getElementById("parallax-5");
+const image6 = document.getElementById("parallax-6");
+const image7 = document.getElementById("parallax-7");
+const image8 = document.getElementById("parallax-8");
+const image9 = document.getElementById("parallax-9");
 
-const onScrolling = ()=> {
-  const offset =window.pageYOffset;
-  image2.style.transform = `translateY(${(offset * .1)}px)`;
-  image3.style.transform = `translateY(${(offset * .2)}px)`;
-  image4.style.transform = `translateY(${(offset * .3)}px)`;
-  image5.style.transform = `translateY(${(offset * .4)}px)`;
-  image6.style.transform = `translateY(${(offset * .5)}px)`;
-  image7.style.transform = `translateY(${(offset * .6)}px)`;
-  image8.style.transform = `translateY(${(offset * .7)}px)`;
-  image9.style.transform = `translateY(${(offset * .8)}px)`;
-}
+const onScrolling = () => {
+  const offset = window.pageYOffset;
+  image2.style.transform = `translate3d(0,${offset * 0.1}px,0)`;
+  image3.style.transform = `translate3d(0,${offset * 0.2}px,0)`;
+  image4.style.transform = `translate3d(0,${offset * 0.3}px,0)`;
+  image5.style.transform = `translate3d(0,${offset * 0.4}px,0)`;
+  image6.style.transform = `translate3d(0,${offset * 0.5}px,0)`;
+  image7.style.transform = `translate3d(0,${offset * 0.6}px,0)`;
+  image8.style.transform = `translate3d(0,${offset * 0.7}px,0)`;
+  image9.style.transform = `translate3d(0,${offset * 0.8}px,0)`;
+};
 
-document.addEventListener("scroll", onScrolling)
+document.addEventListener("scroll", onScrolling);
